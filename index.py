ebd1e6a05746d0d91f38a746975649367e405cdc
Muha-2008 <156815273+Muha-2008@users.noreply.github.com>
2025-06-16 06:38:53 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,23 @@
-x = 25
-if x > 10:
-print ("Yay")
+import requests
+
+def search_unsplash_images(query):
+    access_key = "wUJejRO7znWvme0ASk_AT0nVJF2IT10hYLXVFAZ6F7U"
+    url = "https://api.unsplash.com/search/photos"
+    
+    params = {
+        "query": query,
+        "per_page": 12,
+        "client_id": access_key
+    }
+    
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        for i, result in enumerate(data['results'], 1):
+            print(f"{i}. {result['urls']['regular']}")
+    else:
+        print("Error:", response.status_code, response.text)
+
+# Example
+search_unsplash_images("cats")
